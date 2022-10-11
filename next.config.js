42133229af0851d1c21d55cc70342a79a6212846
Muhammad Royhan <muhroyhan@gmail.com>
2022-10-11 07:56:31 +0700

    change default antd primary color

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withAntdLess = require('next-plugin-antd-less')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents: { ssr: true }
-  }
+  ...withAntdLess({
+    modifyVars: { '@primary-color': '#ff4646' },
+  })
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { SSRProvider } from 'react-bootstrap'
 import '../styles/globals.css'
-import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }) {
     return <SSRProvider>
